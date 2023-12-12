f924dd17c6878b37ea440dbf1518ce77e4ad7cc3
yuhensa <146706367+yuhensa@users.noreply.github.com>
2023-12-12 11:24:01 +0700

    Create pet_name_generator.py

---
+++ pet_name_generator.py
@@ -0,0 +1,4 @@
+print ("Welcome to Pet Name Generator")
+color = input ("What is your favorite color?\n")
+food = input ("What is your favorite food?\n")
+print ("Your pet name is" + color + " " + food)
